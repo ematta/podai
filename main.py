868af8a1c6f3f5852ad0c25e0e1e5436fef6f1c4
Enrique Matta-Rodriguez <enrique.j.matta@gmail.com>
2025-05-18 19:45:29 +0000

    render index.html

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
 app = FastAPI()
+
+app.mount("/assets", StaticFiles(directory="frontend/dist/assets"), name="assets")
 
 @app.get("/health", status_code=200)
 async def health_check():
     return {"status": "ok"}
+
+@app.get("/")
+async def read_index():
+    return FileResponse("frontend/dist/index.html")
