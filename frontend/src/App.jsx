ab4c9403a78fb419e02d17d39f4fac274c1f32b5
Enrique Matta-Rodriguez <enrique.j.matta@gmail.com>
2025-05-18 19:45:19 +0000

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import './App.css';
+
+function App() {
+  return (
+    <Container maxWidth="sm">
+      <Typography variant="h1" component="h1" gutterBottom>
+        PodAI
+      </Typography>
+    </Container>
+  );
+}
+
+export default App;
